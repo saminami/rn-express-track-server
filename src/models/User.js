36950ac4f672490af82ea58afbc7323edd1344ff
sami peralahti <sami.peralahti@vainu.io>
2019-08-10 19:40:41 +0300

    slating and hashing the pw

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bycrypt = require('bcrypt');
 
 // TODO fix unique
 
@@ -13,5 +14,41 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
+
+userSchema.pre('save', function(next) {
+    const user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    bycrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            return next(err);
+        }
+
+        bycrypt.hash(user.password, salt, (err, hash) => {
+            if (err) {
+                return next(err);
+            }
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+    const user = this;
+
+    return new Promise((resolve, reject) => {
+        bycrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+            if (err) {
+                return reject(err);
+            }
+            if (!isMatch) {
+                return reject(false);
+            }
+            return resolve(true);
+        });
+    });
+};
 
 mongoose.model('User', userSchema);
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -16,7 +16,28 @@ router.post('/signup', async (req, res) => {
     } catch (err) {
         return res.status(422).send(err.message);
     }
-    
+});
+
+router.post('/signin', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(422).send({ error: 'Must supply password and email' })
+    }
+
+    const user = await User.findOne({ email })
+
+    if(!user) {
+        return res.status(422).send({error: 'Email not found'});
+    }
+
+    try {
+        await user.comparePassword(password);
+        const token = jwt.sign({ userId: user._id}, 'MY_SECRET_KEY')
+        res.send({ token });
+    } catch (err) {
+        return res.status(422).send({error: 'Invalid password or email'});
+    }
+
 });
 
 module.exports = router;
