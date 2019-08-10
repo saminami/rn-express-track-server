de1a28399c5494d09558b49630422f06f9dac7e8
sami peralahti <sami.peralahti@vainu.io>
2019-08-10 18:17:43 +0300

    mongo setupd

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
+
+const mongoUri =
+    'mongodb+srv://admin:?@cluster0-t2e7t.azure.mongodb.net/test?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+})
+
+mongoose.connection.on('connected', () => {
+    console.log("connected to mongo instance")
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log("Error connecting to mogno", err);
+})
 
 app.get('/', (req, res) => {
     res.send('Hi There')
@@ -9,3 +26,4 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log("listing on port 3k")
 })
+
